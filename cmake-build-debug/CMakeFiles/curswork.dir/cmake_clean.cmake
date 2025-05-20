d5576e53249168382a98783b2b3af2e7f6d7e743
shoumq <johndoe@example.com>
2025-05-20 23:47:59 +0300

    1

---
+++ cmake-build-debug/CMakeFiles/curswork.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curswork.dir/main.cpp.obj"
+  "CMakeFiles/curswork.dir/main.cpp.obj.d"
+  "curswork.exe"
+  "curswork.exe.manifest"
+  "curswork.pdb"
+  "libcurswork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curswork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
